cfaeda37bd4ef4aba2aaeea7a3f8afaf6ebeca24
OmarGallegos1203 <omargallenn52@gmail.com>
2022-06-02 19:08:01 -0500

    Practica 1 - Tarea: Herencia

---
+++ src/main/java/fcfm_s3poo/poo/proyect1st/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fcfm_s3poo.poo.proyect1st;
+
+/**
+ *  Primer ejercicio en clase de Programacion Orientada a Objetos:
+ *  BIENVENIDA A JAVA Y HOLA MUNDO!
+ *  @since 29/01/2022
+ *  @author Omar Gallegos.
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDO A LA PROGRAMACION EN JAVA");
+        System.out.println("H O L A  M U N D O");
+    }
+}
+/*  02/02/2022
+Antes me daba error porque escribí "Sistem" (System) y "printIn" (println)*/
